0a9c10962631641672a695c7ba78253c92228bf3
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2022-09-07 17:12:23 -0300

    fix: homepage margin issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,12 @@ const Home = () => {
       <Head>
         <title>Brieflix</title>
       </Head>
-      <h2 className="text-3xl text-white">Em destaque</h2>
-      <h2 className="text-3xl text-white">Em cartaz</h2>
-      <h2 className="text-3xl text-white">Filmes</h2>
-      <h2 className="text-3xl text-white">Séries</h2>
+      <div className="ml-80 pt-6 pr-6">
+        <h2 className="text-3xl text-white">Em destaque</h2>
+        <h2 className="text-3xl text-white">Em cartaz</h2>
+        <h2 className="text-3xl text-white">Filmes</h2>
+        <h2 className="text-3xl text-white">Séries</h2>
+      </div>
     </Main>
   )
 }
